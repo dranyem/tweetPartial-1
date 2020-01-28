c3ae44120cfa15bc41194c5e7c7eaf4390801a2c
dranyem <davidmeynard@yahoo.com.ph>
2020-01-28 03:04:36 +0000

    Added layout files

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>@yield('title')</title>
 </head>
 <body>
-    @yield('content')
+    <div class="header">This is header</div>
+    <div class="side-bar">This is sidebar</div>
+    <div class="content">This is content
+        @yield('content')</div>
+    <div class="footer">This is footer</div>
+
 </body>
 </html>
